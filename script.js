2c62dd66cb8ad1790a927e9eff97ca579ae2bce9
TimMcCain03 <tm3mcc@gmail.com>
2026-01-17 23:42:21 -0800

    connected to the api server

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // index.js
-console.log('js is working')
+$.ajax({
+  type: 'GET',
+  url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+  dataType: 'json',
+  success: function (response, textStatus) {
+    console.log(response);
+  },
+  error: function (request, textStatus, errorMessage) {
+    console.log(errorMessage);
+  }
+});
