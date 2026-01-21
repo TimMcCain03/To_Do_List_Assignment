69a833e80409616c1fcb0a675b4f6c24f8a673da
TimMcCain03 <tm3mcc@gmail.com>
2026-01-21 13:31:37 -0800

    created new task for to-do-list using an ajax request using personal api key

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // index.js
 $(document).ready(function(){
     $.ajax({
-        type: 'GET',
-        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+        type: 'POST',
+        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=38',
+        contentType: 'application/json',
         dataType: 'json',
+        data: JSON.stringify({
+            task: {
+                content: 'Do somthing fun',
+            }
+        }),
         success: function (response, textStatus) {
-            response.tasks.forEach(function (task) {
-                $('#todo-list').append('<p>' + task.content + '</p>');
-            })
+            console.log(response),
         },
         error: function (request, textStatus, errorMessage) {
             console.log(errorMessage);
