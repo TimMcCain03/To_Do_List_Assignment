81bd3c7afeba811244979c503b9df64b77734032
TimMcCain03 <tm3mcc@gmail.com>
2026-01-21 13:54:20 -0800

    added form that posts items to list in api

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 // index.js
 $(document).ready(function(){
-    $.ajax({
-        type: 'POST',
-        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=38',
-        contentType: 'application/json',
-        dataType: 'json',
-        data: JSON.stringify({
-            task: {
-                content: 'Do somthing fun',
+    var createTask = function () {
+        $.ajax({
+            type: 'POST',
+            url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=38',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify({
+                task: {
+                    content: $('#new-task-content').val()
+                }
+            }),
+            success: function (response, textStatus) {
+                console.log(response);
+            },
+            error: function (request, textStatus, errorMessage) {
+                console.log(errorMessage);
             }
-        }),
-        success: function (response, textStatus) {
-            console.log(response);
-        },
-        error: function (request, textStatus, errorMessage) {
-            console.log(errorMessage);
-        }
+        });  
+    }
+
+    $('#create-task').on('submit', function (e) {
+        e.preventDefault();
+        createTask();
     });
-})
+});
